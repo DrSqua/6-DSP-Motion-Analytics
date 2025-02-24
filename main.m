2de4130a75313ed46c47aace6b6e3d6ddfca75d6
Tobias2512 <hagenaarstobias@gmail.com>
2025-02-24 10:22:38 +0100

    Tobias snelle jelle test code

--- main.m
+++ main.m
@@ -12,4 +12,16 @@ sdxcc
 [b,a] = butter(4,fc/(fs/2));
 
 disp(tsv_data)
-ddd
+
+%% Tobias snelle jelle test code
+% Get the data and seperate the columns
+dataIn = Ax1;
+dataHR = % Column from HR
+dataHL = % Column from HL
+% Filter the data using butterworth filter
+fc = 300; % Cutoff frequency [Hz]
+fs = 1000; % Sampling frequency [Hz]
+n = 4; % Filter order
+
+[b,a] = butter(n, fc/(fs/2)); % Creates filter coefficiënts
+dataOutHR = filter(b,a,dataHR); % Filter the data
