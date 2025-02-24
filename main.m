4ba3b742370df261e9dfc3983037333390d0d8e6
Mateo Van Geyt <mateo.vangeyt@gmail.com>
2025-02-24 10:13:27 +0100

    small changes

--- main.m
+++ main.m
@@ -1,9 +1,15 @@
 % bovenarm, onderarm, torso, bekken hebben assenstelsel nodig
 % x: plain of motion; y: elavation
-frequency = 300;
+fs = 300;
+fc = 1000;
+    
 numberOfFrames = 558;
 numberOfCameras = 11;
 numberOfMarkers = 34;
-    
+
 tsv_data = readtable("10Ax1.tsv", "FileType","text",'Delimiter', '\t');
-print(tsv_data)
+sdxcc
+[b,a] = butter(4,fc/(fs/2));
+
+disp(tsv_data)
+ddd
