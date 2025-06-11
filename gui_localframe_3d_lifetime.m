6f7a137f10c9bc86bb408563703c8701a6b7db1a
Tobias2512 <hagenaarstobias@gmail.com>
2025-06-11 16:27:27 +0200

    Changed full body plot to use the GUI axes

--- gui_localframe_3d_lifetime.m
+++ gui_localframe_3d_lifetime.m
@@ -1,4 +1,4 @@
-function [fig] = gui_localframe_3d_lifetime(tsv, local_frame)
+function gui_localframe_3d_lifetime(tsv, local_frame, ax)
     %GUI_3D_FULLBODY plots skeleton frame for a given t value
     MS  = tsv{:,10:12}; % Nek?
     PX  = tsv{:,13:15}; % Middenrif
@@ -27,7 +27,7 @@ function [fig] = gui_localframe_3d_lifetime(tsv, local_frame)
         case "upper_arm_m1"
             positions = [AR, ELR, EMR];
         case "upper_arm_m2"
-            positions = [AR, ELR, EMR, PLR];
+            positions = [AR, ELR, EMR];
         case 'lower_arm'
             positions = [EMR, PLR, PMR];
         case 'thorax'
@@ -39,14 +39,18 @@ function [fig] = gui_localframe_3d_lifetime(tsv, local_frame)
         case 'shin'
             positions = [CLL, MLL, MML];
         otherwise
-            disp('other value')
+            error("local_frame doesn't match our set local_frames")
     end
     
     % Initialize figure
-    fig = figure('Name', '3D Skeleton for ' + local_frame);
-    axis equal; grid on; rotate3d on;
-    xlabel('X'); ylabel('Y'); zlabel('Z');
-    view(3);
+    cla(ax);
+    hold(ax, 'on');
+    grid(ax, 'on');
+    axis(ax, 'equal');
+    xlabel(ax, 'X');
+    ylabel(ax, 'Y');
+    zlabel(ax, 'Z');
+    view(ax, 3);
     
     % Colors
     colors = ['r', 'y', 'b', 'g'];
@@ -54,50 +58,50 @@ function [fig] = gui_localframe_3d_lifetime(tsv, local_frame)
     % Moving frame throughout
     for i = 1:(length(positions(1, :)) / 3)
         index = 3*(i-1);
-        plot3(positions(:, 1 + index), positions(:, 2 + index), positions(:, 3 + index), colors(i));
-        hold on;
+        plot3(ax, positions(:, 1 + index), positions(:, 2 + index), positions(:, 3 + index), colors(i));
+        hold(ax, 'on');
     end
     
     % Local frame on first and last position
     % Upper arm
     pos1 = [AR(1, :); ELR(1, :); EMR(1, :); AR(1, :)];
     pos2 = [AR(end, :); ELR(end, :); EMR(end, :); AR(end, :)];
-    plot3(pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
-    plot3(pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
+    plot3(ax, pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
+    plot3(ax, pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
     % Lower arm
     pos1 = [EMR(1, :); PLR(1, :); PMR(1, :); EMR(1, :)];
     pos2 = [EMR(end, :); PLR(end, :); PMR(end, :); EMR(end, :)];
-    plot3(pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
-    plot3(pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
+    plot3(ax, pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
+    plot3(ax, pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
     % Thorax
     pos1 = [AR(1, :); MS(1, :); AL(1, :); PX(1, :); AR(1, :)];
     pos2 = [AR(end, :); MS(end, :); AL(end, :); PX(end, :); AR(end, :)];
-    plot3(pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
-    plot3(pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
+    plot3(ax, pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
+    plot3(ax, pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
     % Heup (pelvis)
     pos1 = [SIPSR(1, :); SIPSL(1, :); SIASL(1, :); SIASR(1, :); SIPSR(1, :)];
     pos2 = [SIPSR(end, :); SIPSL(end, :); SIASL(end, :); SIASR(end, :); SIPSR(end, :)];
-    plot3(pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
-    plot3(pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
+    plot3(ax, pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
+    plot3(ax, pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
     % Linker dijbeen
     pos1 = [CLL(1, :); SIPSL(1, :); SIASL(1, :); CLL(1, :)];
     pos2 = [CLL(end, :); SIPSL(end, :); SIASL(end, :); CLL(end, :)];
-    plot3(pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
-    plot3(pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
+    plot3(ax, pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
+    plot3(ax, pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
     % Linker scheenbeen
     pos1 = [CLL(1, :); MLL(1, :); MML(1, :); CLL(1, :)];
     pos2 = [CLL(end, :); MLL(end, :); MML(end, :); CLL(end, :)];
-    plot3(pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
-    plot3(pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
+    plot3(ax, pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
+    plot3(ax, pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
     % Rechter dijbeen
     pos1 = [CLR(1, :); SIPSR(1, :); SIASR(1, :); CLR(1, :)];
     pos2 = [CLR(end, :); SIPSR(end, :); SIASR(end, :); CLR(end, :)];
-    plot3(pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
-    plot3(pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
+    plot3(ax, pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
+    plot3(ax, pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
     % Rechter scheenbeen
     pos1 = [CLR(1, :); MLR(1, :); MMR(1, :); CLR(1, :)];
     pos2 = [CLR(end, :); MLR(end, :); MMR(end, :); CLR(end, :)];
-    plot3(pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
-    plot3(pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
+    plot3(ax, pos1(:, 1), pos1(:, 2), pos1(:, 3), '-k')
+    plot3(ax, pos2(:, 1), pos2(:, 2), pos2(:, 3), '-k')
     
-    hold off;
+    hold(ax, 'off');
